96772e08cf782f652427e42dcadb5f222b49fb6c
Michael Rodriguez <michael_rios05@hotmail.com>
2024-03-31 17:53:33 -0500

    Funcionamiento basico con datos del pokemon e incluido el mensaje de error

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $("#mi-boton").on("click", function () {
         var pokemonName = $("#txt-buscar").val().toLowerCase();
+
+        $("#error-message").hide();
         $.ajax({
             url: "https://pokeapi.co/api/v2/pokemon/" + pokemonName,
             type: "GET",
@@ -8,14 +10,33 @@ $(document).ready(function () {
             contentType: "application/json",
             success: function (data) {
                 // Actualizar imagen
-                $("#pokemon-image").html(`<img src="${data.sprites.other.home.front_default}" id="poke-img">`);
+                $("#poke-image").html(`<img src="${data.sprites.other.home.front_default}" id="poke-img">`);
 
                 // Actualizar otros datos
-                $("#pokemon-name").text(data.name.toUpperCase());
-                $("#pokemon-type").text(data.types.map(type => type.type.name).join(', '));
-                $("#pokemon-height").text(`${data.height / 10} m`);
-                $("#pokemon-weight").text(`${data.weight / 10} kg`);
-                $("#pokemon-abilities").text(data.abilities.map(ability => ability.ability.name).join(', '));
+                $("#poke-name").text(data.name.toUpperCase());
+                $("#poke-id").text(data.id);
+                $("#poke-type").text(data.types.map(type => type.type.name).join(', '));
+                $("#poke-height").text(`${data.height / 10} m`);
+                $("#poke-weight").text(`${data.weight / 10} kg`);
+                $("#poke-abilities").text(data.abilities.map(ability => ability.ability.name).join(', '));
+
+                // Actualizar estadísticas
+                $("#poke-stats").empty();
+                data.stats.forEach(stat => {
+                    $("#poke-stats").append($("<tr>").append($("<td>").text(stat.stat.name.toUpperCase()), $("<td>").text(stat.base_stat)));
+                });
+                /*data.stats.forEach(stat => {
+                    $("#poke-stats").append($("<li>").text(`${stat.stat.name}: ${stat.base_stat}`));
+                });*/
+            },
+            error: function (xhr, status, error) {
+                // Manejar error 404 (Pokémon no encontrado)
+                if (xhr.status === 404) {
+                    const errorMessage = `<strong>Error 404:</strong> El Pokémon "${pokemonName}" no fue encontrado.`;
+                    $("#error-message").html(errorMessage).show();
+                } else {
+                    console.error("Error al obtener datos del Pokémon:", error);
+                }
             }
         });
     });
