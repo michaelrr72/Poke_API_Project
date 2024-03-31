e23357b0b987f9f395f5e10d2eafec1d0a678398
Michael Rodriguez <michael_rios05@hotmail.com>
2024-03-31 10:35:44 -0500

    Cambio por imagen normal con tamaño 150pxditto

--- js/main.js
+++ js/main.js
@@ -11,12 +11,10 @@ $(document).ready(function () {
                 //console.log(data.sprites.other.showdown.front_default) // Gif
                 // Crea un nuevo elemento de imagen
                 var imgElement = $('<img>', {
-                    src: data.sprites.other.showdown.front_default, // Ruta de la imagen
+                    src: data.sprites.other.home.front_default, // Ruta de la imagen
                     width: '150px' // Tamaño de la imagen
                 });
-                // Limpia el contenido anterior y añade el nuevo elemento de imagen al div
-                //$("#pokemon-image").empty().append(imgElement);
-                //$("#pokemon-image").html(`<img src="${data.sprites.other.showdown.front_default}" width= '150px'>`)
+                //$("#pokemon-image").html(`<img src="${data.sprites.other.showdown.front_default}">`)
                 $("#pokemon-image").html(imgElement)
             }
         })
